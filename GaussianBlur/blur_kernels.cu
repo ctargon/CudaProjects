34d1f6c90e65a4946286675edf5cdfc49f3066d1
ctargon <ctargon@g.clemson.edu>
2019-02-12 14:34:21 -0500

    comments

--- GaussianBlur/blur_kernels.cu
+++ GaussianBlur/blur_kernels.cu
@@ -86,6 +86,7 @@ void recombineChannels(unsigned char *d_r, unsigned char *d_g, unsigned char *d_
 		int i = col * numCols + row;
 
 		//d_orgba[i] = make_uchar4(d_r[i], d_g[i], d_b[i], 255);
+		// save the output as bgr, not rgb, because opencv defaults to that
 		d_orgba[i] = make_uchar4(d_b[i], d_g[i], d_r[i], 255); 
 	}
 } 
